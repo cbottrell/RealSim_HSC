f77790b65d99db1d3c03b394cd65ea39e6c89adc
Connor Bottrell <connor.bottrell@gmail.com>
2021-07-19 17:00:26 +0900

    Deleting old files.

--- Scripts/example_image.py
+++
@@ -1,6 +0,0 @@
-import hsc_image
-
-object_id,ra,dec='36411448540270969',30.611445,-6.475494
-out_dir='/lustre/work/connor.bottrell/RealSim_HSC/Cutouts'
-
-hsc_image.get_cutouts(object_id,ra,dec,out_dir,dr='dr4',rerun='s21a_wide',filters='GRIZY',fov_arcsec=120)
